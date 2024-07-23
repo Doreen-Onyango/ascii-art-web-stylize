2aba9f2e5236176bff66272725914052800b74ba
khahussein <kherld.hussein@gmail.com>
2024-07-23 18:20:20 +0300

    Fix: Implement better files handler. This captures .html, .css files inside the template folder. Handle errors if page not found

--- server/route_handler.go
+++ server/route_handler.go
@@ -9,19 +9,22 @@ import (
 )
 
 func Handl(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	path := filepath.Join("..", "templates", "index.html")
-	tmpl, err := template.ParseFiles(path)
-	if err != nil {
-		send.SendError(w, "Error 404: PAGE NOT FOUND", http.StatusNotFound)
-		return
-	}
+	if r.URL.Path == "/" {
+		path := filepath.Join("..", "templates", "index.html")
+		tmpl, err := template.ParseFiles(path)
+		if err != nil {
+			send.SendError(w, "Error 404: PAGE NOT FOUND", http.StatusNotFound)
+			return
+		}
 
-	if err := tmpl.Execute(w, nil); err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		if err := tmpl.Execute(w, nil); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	} else if r.URL.Path == "/styles.css" {
+		cssPath := filepath.Join("..", "templates", "styles.css")
+		http.ServeFile(w, r, cssPath)
+	} else {
+		http.NotFound(w, r)
 	}
 }
